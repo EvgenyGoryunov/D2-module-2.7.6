796121b8851a4ae4a83377007a70087c43800a68
Evgeny <ges1987@list.ru>
2021-12-06 16:27:58 +0700

    2 - vse krome vivoda imeni faila

--- newapp/models.py
+++ newapp/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import Sum
+from django.core.validators import MinValueValidator
 
 
 # Модель, содержащая объекты всех авторов.
--- newapp/views.py
+++ newapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Author, Category, Post, Comment
 
-# Create your views here.
+
+class PostsList(ListView):
+    model = Post  # указываем модель, объекты которой мы будем выводить
+    template_name = 'posts.html'  # указываем имя шаблона, в котором будет лежать HTML,
+    # в котором будут все инструкции о том, как именно пользователю должны вывестись наши объекты
+    context_object_name = 'posts'  # это имя списка, в котором будут лежать все объекты, его надо указать,
+    # чтобы обратиться к самому списку объектов через HTML-шаблон
