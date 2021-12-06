00fb74049423b53452b7be450fbf568d8f4c7981
Evgeny <ges1987@list.ru>
2021-12-06 16:38:13 +0700

    3 - vse krome vivoda imeni faila

--- newapp/urls.py
+++ newapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PostsList  # импортируем наше представление
+from .views import PostsList, PostsDetail  # импортируем наше представление
 
 urlpatterns = [
     # path — означает путь. В данном случае путь ко всем товарам у нас останется пустым, позже станет ясно, почему
     path('', PostsList.as_view()),
     # т. к. сам по себе это класс, то нам надо представить этот класс в виде view. Для этого вызываем метод as_view
+    path('<int:pk>', PostsDetail.as_view()),  # pk — это первичный ключ товара, который будет выводиться
+    # у нас в шаблон
 ]
--- newapp/views.py
+++ newapp/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Author, Category, Post, Comment
 
 
@@ -8,3 +8,10 @@ class PostsList(ListView):
     # в котором будут все инструкции о том, как именно пользователю должны вывестись наши объекты
     context_object_name = 'posts'  # это имя списка, в котором будут лежать все объекты, его надо указать,
     # чтобы обратиться к самому списку объектов через HTML-шаблон
+
+
+# создаём представление, в котором будут детали конкретного отдельного товара
+class PostsDetail(DetailView):
+    model = Post  # модель всё та же, но мы хотим получать детали конкретно отдельного товара
+    template_name = 'post.html'  # название шаблона будет product.html
+    context_object_name = 'post'  # название объекта
