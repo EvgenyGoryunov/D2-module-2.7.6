ee63521476d7b6bfe6f6faa9ab790f2e3dcc7977
Evgeny <ges1987@list.ru>
2021-12-07 18:15:44 +0700

    15 вывод списка статей задом на перед 14 мелкие редактирования 13 макс вывод инфы на одиночной странице 12 добавлена ссылка на главную надпись 11 выведен правильный формат заголовок, дата, и текст 20 слов 10 добавил красивые стили вывода текста

--- newapp/views.py
+++ newapp/views.py
@@ -10,6 +10,9 @@ class PostsList(ListView):
     # в котором будут все инструкции о том, как именно пользователю должны вывестись наши объекты
     context_object_name = 'posts'  # это имя списка, в котором будут лежать все объекты, его надо указать,
     # чтобы обратиться к самому списку объектов через HTML-шаблон
+    queryset = Post.objects.order_by('-dateCreation')
+
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -17,6 +20,7 @@ class PostsList(ListView):
         context['value1'] = None  # добавим ещё одну пустую переменную, чтобы на её примере посмотреть работу
         # другого фильтра
         return context
+
 
 
 # создаём представление, в котором будут детали конкретного отдельного товара
