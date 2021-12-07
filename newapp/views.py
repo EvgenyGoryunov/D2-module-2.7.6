deaf5cd2911579537313d080d3c191306767e7ec
Evgeny <ges1987@list.ru>
2021-12-07 13:29:48 +0700

    14 мелкие редактирования 13 макс вывод инфы на одиночной странице 12 добавлена ссылка на главную надпись 11 выведен правильный формат заголовок, дата, и текст 20 слов 10 добавил красивые стили вывода текста

--- newapp/views.py
+++ newapp/views.py
@@ -14,7 +14,8 @@ class PostsList(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['time_now'] = datetime.utcnow()  # добавим переменную текущей даты time_now
-        context['value1'] = None  # добавим ещё одну пустую переменную, чтобы на её примере посмотреть работу другого фильтра
+        context['value1'] = None  # добавим ещё одну пустую переменную, чтобы на её примере посмотреть работу
+        # другого фильтра
         return context
 
 
